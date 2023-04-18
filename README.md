1e7ee5dbaa04bb9df5deaea5ff38bd843c3c67a3
root@pmr1 <root@pmr1>
2023-04-18 17:48:30 +0300

    whatnode moved to nobudget

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
-# Definitively Not a Cloud
+# Definitely Not a Cloud
 
 _wrapper scripts for DRBD/XEN resource management_
+
+_tested on slack150_
+
+## Requirements
+
+- DRBD with LVM2 thin provisioning
+- XEN
+
+## Install
+
+	git clone https://github.com/pbraun9/dnc
+	cd dnc/
+	make install
+
+## Setup
+
+	cp dnc.conf /etc/
+	vi /etc/dnc.conf
 
--- dnclib.bash
+++ dnclib.bash
@@ -29,15 +29,6 @@ function bomb {
 	exit 1
 }
 
-# defines $node
-function whatnode {
-	[[ -z $guest ]] && bomb function $0 requires \$guest
-
-	node=`dsh -e -g xen "xl list $guest 2>&1 | sed 1d | cut -f1 -d' '| grep -v 'rc=-6'" | cut -f1 -d:`
-
-	[[ -z $node ]] && bomb guest $guest does not seem to be running anywhere in the farm
-}
-
 # defines $ip
 function dec2ip {
 	[[ -z $prefix ]] && echo function dec2ip requires \$prefix from /etc/dnc.conf && exit 1
